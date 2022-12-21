db06b529556930bc17b4ad7bb7050438ef014c42
jisjesuscova <116112886+jisjesuscova@users.noreply.github.com>
2022-12-21 07:47:02 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from config import DevConfig
+from config import ProConfig
 from flask_migrate import Migrate
 from flask_login import LoginManager, current_user, logout_user
 from functools import wraps
 
 app = Flask(__name__)
-app.config.from_object(DevConfig)
+app.config.from_object(ProConfig)
 
 csrf_protect = CSRFProtect(app)
 
