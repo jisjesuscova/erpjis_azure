261b41649161da768c6b7c86fc4847bf6427fdab
jisjesuscova <116112886+jisjesuscova@users.noreply.github.com>
2022-12-21 08:21:55 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
-from flask import Flask, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
-from config import ProConfig
-from flask_migrate import Migrate
-from flask_login import LoginManager, current_user, logout_user
-from functools import wraps
-
+from flask import Flask
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
