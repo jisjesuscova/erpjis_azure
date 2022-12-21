c402472fefc90fec91ffadf5925bd18db7bb36b4
jisjesuscova <jesuscova@jisparking.com>
2022-12-21 11:53:03 -0300

    cambios

--- app/civil_states/controllers.py
+++ app/civil_states/controllers.py
@@ -2,10 +2,7 @@ from flask import Blueprint, render_template
 
 civil_state = Blueprint("civil_states", __name__)
 
-@civil_state.before_request
-def constructor():
-   pass
 
 @civil_state.route("/master_data/civil_states", methods=['GET'])
 def index():
-   return 
+   return "Probando"
