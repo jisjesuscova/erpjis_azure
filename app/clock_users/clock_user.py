451917a526cfd85d4fbbb5a31dc06be53142994e
jisjesuscova <jesuscova@jisparking.com>
2022-12-28 17:35:21 -0300

    cambios

--- app/clock_users/clock_user.py
+++ app/clock_users/clock_user.py
@@ -4,7 +4,16 @@ from datetime import datetime
 
 class ClockUser():
     def store(data):
+        clock_user = ClockUserModel()
+        clock_user.uid = data['uid']
+        clock_user.rut = data['rut']
+        clock_user.full_name = data['full_name']
+        clock_user.privilege = data['privilege']
+        clock_user.added_date = datetime.now()
+        clock_user.updated_date = datetime.now()
 
+        db.session.add(clock_user)
+        db.session.commit()
         
         return str(data['uid']) + "_" + str(data['rut']) + "_" + str(data['full_name']) + "_" + str(data['privilege'])
  
