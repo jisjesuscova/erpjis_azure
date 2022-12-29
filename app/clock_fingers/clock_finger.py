d69c56aea0f8b7e75859a3869801aa8d93fdb37f
jisjesuscova <jesuscova@jisparking.com>
2022-12-29 15:52:43 -0300

    cambios

--- app/clock_fingers/clock_finger.py
+++ app/clock_fingers/clock_finger.py
@@ -3,6 +3,30 @@ from app import db
 from datetime import datetime
 
 class ClockFinger():
+    @staticmethod
+    def to_json(data):
+        res = []
+
+        for row in data:
+            res.append({
+                'uid': row.uid,
+                'template': row.template
+            })
+
+        return res
+
+    @staticmethod
+    def get():
+        clock_fingers = ClockFingerModel.query.all()
+
+        return clock_fingers
+
+    @staticmethod
+    def check(data):
+        quantity = ClockFingerModel.query.filter_by(uid=data['uid']).count()
+
+        return quantity
+
     def store(data):
         clock_finger = ClockFingerModel()
         clock_finger.uid = data['uid']
@@ -14,4 +38,15 @@ class ClockFinger():
         db.session.commit()
         
         return str(data['uid']) + "_" + str(data['template'])
- 
+    
+    @staticmethod
+    def update(data):
+        clock_finger = ClockFingerModel.query.filter_by(uid = data['uid']).first()
+        clock_finger.uid = data['uid']
+        clock_finger.template = data['template']
+        clock_finger.updated_date = datetime.now()
+
+        db.session.add(clock_finger)
+        db.session.commit()
+
+        return str(data['uid']) + "_" + str(data['template'])
