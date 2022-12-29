5347e9ea3d4a429ba212bda9f628d42c187f9b55
jisjesuscova <jesuscova@jisparking.com>
2022-12-29 12:20:29 -0300

    cambios

--- app/clock_users/clock_user.py
+++ app/clock_users/clock_user.py
@@ -1,8 +1,29 @@
 from app.models.models import ClockUserModel
 from app import db
 from datetime import datetime
+from flask import json
 
 class ClockUser():
+    @staticmethod
+    def to_json(data):
+        res = []
+
+        for row in data:
+            res.append({
+                'uid': row.uid,
+                'rut': row.rut,
+                'full_name': row.full_name,
+                'privilege': row.privilege
+            })
+
+        return res
+
+    @staticmethod
+    def get():
+        clock_users = ClockUserModel.query.all()
+
+        return clock_users
+
     def store(data):
         clock_user = ClockUserModel()
         clock_user.uid = data['uid']
--- app/clock_users/controllers.py
+++ app/clock_users/controllers.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request
 from app.clock_users.clock_user import ClockUser
+from app.helpers.helper import Helper
+from flask import jsonify
+import json
 
 clock_user = Blueprint("clock_users", __name__)
 
@@ -9,3 +12,12 @@ def store():
    data = ClockUser.store(request.form)
 
    return str(data)
+
+@clock_user.route("/clock_user", methods=['GET'])
+def index():
+   
+   data = ClockUser.get()
+
+   res = ClockUser.to_json(data)
+
+   return json.dumps(res)
