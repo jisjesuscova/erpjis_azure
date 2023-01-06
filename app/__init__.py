fce02af7011acd33dbe8e24cb5392c5c31d94d36
jisjesuscova <jesuscova@jisparking.com>
2023-01-06 15:28:22 -0300

    cambios

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, g
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
