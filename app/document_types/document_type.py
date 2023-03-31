7d0c49ee6c019c1167476a0d80123013eb775e11
jisjesuscova <jesuscova@jisparking.com>
2023-03-31 10:11:04 -0300

    cambios

--- app/document_types/document_type.py
+++ app/document_types/document_type.py
@@ -7,7 +7,7 @@ class DocumentType():
     @staticmethod
     def get(id = '', group_id = '', field = '', value = '', special_id = []):
         if id == '':
-            document_type = DocumentTypeModel.query.filter_by(document_group_id = group_id).order_by('document_type').all()
+            document_type = DocumentTypeModel.query.filter_by(document_group_id = group_id).order_by('order').all()
         else:
             document_type = DocumentTypeModel.query.get(id)
 
