838066e3f95a629e4e682ec59dcf5ff567e412f1
jisjesuscova <jesuscova@jisparking.com>
2023-05-10 11:07:49 -0400

    cambios

--- app/clock_attendances/clock_attendance.py
+++ app/clock_attendances/clock_attendance.py
@@ -4,18 +4,6 @@ from app.clock_users.clock_user import ClockUser
 
 class ClockAttendance():
     def store(data):
-        print(data['rut'])
-        clock_user = ClockUser.get(data['rut'])
 
-        clock_attendance = ClockAttendanceModel()
-        clock_attendance.uid = clock_user.uid
-        clock_attendance.rut = data['rut']
-        clock_attendance.punch = data['punch']
-        clock_attendance.status = data['status']
-        clock_attendance.mark_date = data['mark_date']
-        clock_attendance.branch_office_id = data['branch_office_id']
-
-        db.session.add(clock_attendance)
-        db.session.commit()
         
         return str(clock_user.uid) + "_" + str(data['rut']) + "_" + str(data['punch']) + "_" + str(data['status']) + "_" + str(data['mark_date']) + "_" + str(data['branch_office_id'])
--- app/clock_attendances/controllers.py
+++ app/clock_attendances/controllers.py
@@ -5,7 +5,6 @@ clock_attendance = Blueprint("clock_attendances", __name__)
 
 @clock_attendance.route("/clock_attendance/store", methods=['GET', 'POST'])
 def store():
-   
    data = ClockAttendance.store(request.form)
 
    return str(data)
