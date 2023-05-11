c6dbff1ac59e7d6c1b0da13bcc370632e4563cca
jisjesuscova <jesuscova@jisparking.com>
2023-05-11 12:46:49 -0400

    cambios

--- app/clock_attendances/clock_attendance.py
+++ app/clock_attendances/clock_attendance.py
@@ -4,6 +4,17 @@ from app.clock_users.clock_user import ClockUser
 
 class ClockAttendance():
     def store(data):
-        print(data['rut'])
-        
+        clock_user = ClockUser.get(data['rut'])
+        uid = clock_user.uid
+
+        clock_attendance = ClockAttendanceModel()
+        clock_attendance.uid = uid
+        clock_attendance.rut = data['rut']
+        clock_attendance.punch = data['punch']
+        clock_attendance.status = data['status']
+        clock_attendance.mark_date = data['mark_date']
+        clock_attendance.branch_office_id = data['branch_office_id']
+        db.session.add(clock_attendance)
+        db.session.commit()
+
         return str(data)
