67b180c1088dcb6d6c533d0ebed79e28148a87ba
jisjesuscova <jesuscova@jisparking.com>
2023-05-11 16:22:38 -0400

    cambios

--- app/clocks/clock.py
+++ app/clocks/clock.py
@@ -1,8 +1,29 @@
-from app.models.models import ClockModel
+from app.models.models import ClockModel, BranchOfficeModel
 from app import db
 from datetime import datetime
+from sqlalchemy import cast, DateTime
 
 class Clock():
+    @staticmethod
+    def get(page):
+        clock_data = db.session.query(ClockModel.id, ClockModel.sn, BranchOfficeModel.branch_office, ClockModel.ip, ClockModel.updated_date)\
+                        .join(BranchOfficeModel, ClockModel.branch_office_id == BranchOfficeModel.id)\
+                        .paginate(page=page, per_page=20, error_out=False)
+
+        return clock_data
+
+    @staticmethod
+    def delete(id):
+        clock = ClockModel.query.filter_by(id=id).first()
+
+        db.session.delete(clock)
+        try:
+            db.session.commit()
+
+            return clock
+        except Exception as e:
+            return {'msg': 'Data could not be stored'}
+        
     @staticmethod
     def check(data):
         quantity = ClockModel.query.filter_by(sn=data['sn']).count()
--- app/clocks/controllers.py
+++ app/clocks/controllers.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template, redirect, url_for, flash, session, jsonify
 from app.clocks.clock import Clock
 
 clock = Blueprint("clocks", __name__)
+
+@clock.route("/clock_data", methods=['GET'])
+def index(page = 1):
+   clock_data = Clock.get(page)
+
+   title = 'Relojes'
+
+   module_name = 'Gestión Tiempo'
+
+   return render_template('human_resource/clocks/clock_data.html', clock_data = clock_data, title = title, module_name = module_name)
 
 @clock.route("/clock/data", methods=['GET', 'POST'])
 def store():
@@ -13,3 +23,11 @@ def store():
       data = Clock.update(request.form)
 
    return str(data)
+
+@clock.route("/clock/delete/<int:id>", methods=['GET'])
+def delete(id):
+    Clock.delete(id)
+
+    flash("El reloj ha sido borrado con éxito.", "success")
+
+    return redirect(url_for('clocks.index'))
